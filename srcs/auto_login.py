70ead2c1b3d6ccb83745c9901c53f91ebf9426ee
yuto1009 <bmwb71009@gmail.com>
2023-12-16 21:36:53 +0900

    add: 一応動くぜ

--- srcs/auto_login.py
+++ srcs/auto_login.py
@@ -1,7 +1,8 @@
 from playwright.sync_api import Playwright, sync_playwright
 
-username = "github_master"
-password = "42tokyo"
+
+# username = "github_master"
+# password = "42tokyo"
 
 def locate(page):
     # フォローを最後までスクロールする
@@ -27,14 +28,6 @@ def locate(page):
             break  
     
         previous_count = current_count
-    # ユーザー名の取得
-    # following_locator = page.locator('body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1)')
-    # followingUsersElements = following_locator.locator('.x1a02dak.x1q0g3np.xdl72j9 a[role="link"]')
-    # page.wait_for_timeout(500)  # Adjust the timeout as needed
-
-    # for followingUsersElement in followingUsersElements.all():
-    #     userName = followingUsersElement.inner_text()
-    #     userNames.append(userName)
 
     return userNames
 
@@ -46,9 +39,36 @@ def find_user(page, username):
     page.wait_for_timeout(5000)  # Adjust the timeout as needed
     return(locate(page))
 
+def find_unique_followers(a_list, b_list, my_list, page):
+	# 4. 2次元リスト x_list を作成
+	x_list = []
+
+	for account in a_list:
+		if account in b_list and account not in my_list:
+			x_list.append([account])
+
+	for j in range(len(x_list)):
+		followers = find_user(page, x_list[j])
+		x_list[j].append(followers)
+	print(x_list)
+		
+	y_list = []
+	for _, followers in x_list:
+		for follower in followers:
+			if follower not in a_list and follower not in b_list and follower not in my_list:
+				y_list.append(follower)
+
+	#順序を保持する
+	unique_list = []
+	for item in y_list:
+		if item not in unique_list:
+			unique_list.append(item)
+
+	y_list = unique_list
+	return y_list
 
 
-def run(playwright: Playwright) -> None:
+def run(playwright: Playwright, username, password, user1, user2) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
 
@@ -79,36 +99,19 @@ def run(playwright: Playwright) -> None:
     # フォローしているユーザーのリストを表示
     page.goto(f"https://www.instagram.com/{username}/following/")
 
-    # previous_count = 0
-    # while True:
-    #     # フォローページの末尾にいくまでスクロールする
-    #     page.locator('a > div > div > span[dir="auto"]').last.scroll_into_view_if_needed()
-    #     page.wait_for_timeout(2000)  # Adjust the timeout as needed
-
-    #     # 現在までにロードしたフォロー数を数える
-    #     current_count = page.locator('a > div > div > span[dir="auto"]').count()
-
-    #     # スクロールの前後でカウント数が更新されているか確認、更新がなかったらスクロールをやめる
-    #     if current_count == previous_count:
-    #         break  
-    
-    #     previous_count = current_count
-
-    # # ユーザーのフォローを配列で取得
-    # user_follows = locate(page)
-    # print(user_follows)
-    
     # ユーザーA、ユーザーBの情報を得る
     user_self = find_user(page, username)
-    user_a = find_user(page, "susasaki")
-    user_b = find_user(page, "github_master")
+    user_a = find_user(page, user1)
+    user_b = find_user(page, user2)
     print(user_self)
     print(user_a)
     print(user_b)
 
+    unique_followers = find_unique_followers(user_a, user_b, user_self,page)
+    print(unique_followers)
     # ブラウザを閉じる
     browser.close()
 
-if __name__ == "__main__":
+def run_playwright(username, password, insta1, insta2):
     with sync_playwright() as pw:
-        run(pw)
+        run(pw, username, password, insta1, insta2)
