62f6ca8aa8baab24115078bd25da690de5074ff3
yuto1009 <bmwb71009@gmail.com>
2023-12-16 20:20:34 +0900

    update: 自分、ユーザー１、ユーザー２のfollowin(一部)の取得が可能

--- auto_login.py
+++ auto_login.py
@@ -1,19 +1,50 @@
 from playwright.sync_api import Playwright, sync_playwright
 
-username = "yuendo42"
+username = "github_master"
 password = "42tokyo"
 
 def locate(page):
-    # ユーザー名の取得
-    following_locator = page.locator('body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1)')
-    followingUsersElements = following_locator.locator('.x1a02dak.x1q0g3np.xdl72j9 a[role="link"]')
-    
+    # フォローを最後までスクロールする
+    previous_count = 0
     userNames = []
-    for followingUsersElement in followingUsersElements.all():
-        userName = followingUsersElement.inner_text()
-        userNames.append(userName)
+    while True:
+        # フォローページの末尾にいくまでスクロールする
+        page.locator('a > div > div > span[dir="auto"]').last.scroll_into_view_if_needed()
+
+        # ユーザー名の取得
+        following_locator = page.locator('body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1)')
+        followingUsersElements = following_locator.locator('.x1a02dak.x1q0g3np.xdl72j9 a[role="link"]')
+        for followingUsersElement in followingUsersElements.all():
+            userName = followingUsersElement.inner_text()
+            userNames.append(userName)
+        page.wait_for_timeout(2000)  # Adjust the timeout as needed
+
+        # 現在までにロードしたフォロー数を数える
+        current_count = page.locator('a > div > div > span[dir="auto"]').count()
+
+        # スクロールの前後でカウント数が更新されているか確認、更新がなかったらスクロールをやめる
+        if current_count == previous_count:
+            break  
+    
+        previous_count = current_count
+    # ユーザー名の取得
+    # following_locator = page.locator('body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1)')
+    # followingUsersElements = following_locator.locator('.x1a02dak.x1q0g3np.xdl72j9 a[role="link"]')
+    # page.wait_for_timeout(500)  # Adjust the timeout as needed
+
+    # for followingUsersElement in followingUsersElements.all():
+    #     userName = followingUsersElement.inner_text()
+    #     userNames.append(userName)
 
     return userNames
+
+
+
+def find_user(page, username):
+    # フォローしているユーザーのリストを表示
+    page.goto(f"https://www.instagram.com/{username}/following/")
+    page.wait_for_timeout(5000)  # Adjust the timeout as needed
+    return(locate(page))
 
 
 
@@ -46,31 +77,35 @@ def run(playwright: Playwright) -> None:
     page.wait_for_url("https://www.instagram.com/")
 
     # フォローしているユーザーのリストを表示
-    page_to_go = f"https://www.instagram.com/{username}/following/"
-    page.goto(page_to_go)
+    page.goto(f"https://www.instagram.com/{username}/following/")
 
-    previous_count = 0
-    while True:
-        # フォローページの末尾にいくまでスクロールする
-        page.locator('a > div > div > span[dir="auto"]').last.scroll_into_view_if_needed()
-        page.wait_for_timeout(2000)  # Adjust the timeout as needed
+    # previous_count = 0
+    # while True:
+    #     # フォローページの末尾にいくまでスクロールする
+    #     page.locator('a > div > div > span[dir="auto"]').last.scroll_into_view_if_needed()
+    #     page.wait_for_timeout(2000)  # Adjust the timeout as needed
 
-        # 現在までにロードしたフォロー数を数える
-        current_count = page.locator('a > div > div > span[dir="auto"]').count()
+    #     # 現在までにロードしたフォロー数を数える
+    #     current_count = page.locator('a > div > div > span[dir="auto"]').count()
 
-        # スクロールの前後でカウント数が更新されているか確認、更新がなかったらスクロールをやめる
-        if current_count == previous_count:
-            break  
+    #     # スクロールの前後でカウント数が更新されているか確認、更新がなかったらスクロールをやめる
+    #     if current_count == previous_count:
+    #         break  
     
-        previous_count = current_count
+    #     previous_count = current_count
 
-    # ユーザーのフォローを配列で取得
-    user_follows = locate(page)
-    print(user_follows)
+    # # ユーザーのフォローを配列で取得
+    # user_follows = locate(page)
+    # print(user_follows)
     
-    # for friend in user_follows:
+    # ユーザーA、ユーザーBの情報を得る
+    user_self = find_user(page, username)
+    user_a = find_user(page, "susasaki")
+    user_b = find_user(page, "github_master")
+    print(user_self)
+    print(user_a)
+    print(user_b)
 
-    
     # ブラウザを閉じる
     browser.close()
 
