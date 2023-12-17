740b6acbdbfea8267bc84ba6fac38961d2c25716
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-12-17 13:36:39 +0900

    bug: pythonでwindowを表示させるから、GUIが表示できる環境を整える必要がある。

--- pakages.sh
+++ pakages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+IMAGE_NAME="test-image:v1"
+PROJECT_PATH=$(cd $(dirname $0); pwd)
 
-echo "Installing playwright..."
+# イメージのビルド
+docker image build -t "$IMAGE_NAME" ./docker
 
-pip install playwright
-playwright install
-
-echo "Installation complete."
+# コンテナの作成　プロジェクトパスにバインド
+docker run -it -v ${PROJECT_PATH}:/home "$IMAGE_NAME" /bin/bash -c "Xvfb :99 & export DISPLAY=:99 && /bin/bash"
--- start-docker.sh
+++ start-docker.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 
 IMAGE_NAME="test-image:v1"
 PROJECT_PATH=$(cd $(dirname $0); pwd)
+CONTAINER_NAME="my_python_container"
 
 # イメージのビルド
 docker image build -t "$IMAGE_NAME" ./docker
 
-# コンテナの作成　プロジェクトパスにバインド
-docker run -it -v ${PROJECT_PATH}:/home "$IMAGE_NAME" /bin/bash
+# 既存のコンテナを確認し、存在する場合は削除
+if [ $(docker ps -aq -f name=^/${CONTAINER_NAME}$) ]; then
+    docker rm -f ${CONTAINER_NAME}
+fi
+
+# コンテナの作成　プロジェクトパスにバインドし、SSHポートフォワーディングを設定
+docker run -d --name ${CONTAINER_NAME} -p 2222:22 --workdir /home -v ${PROJECT_PATH}:/home "$IMAGE_NAME"
